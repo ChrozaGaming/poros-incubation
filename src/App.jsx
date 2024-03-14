9bbf551d06200ce35305e2bb77c24a89853d11e3
ChrozaGaming <hilmyraihankindy@gmail.com>
2024-03-15 00:34:36 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// src/App.jsx
+import React from 'react';
+import Header from './components/Header';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import './style.css';
+
+const App = () => (
+    <div>
+        <Header />
+        <div className="container">
+            <div className="card-small-container">
+                <Card className="card-small card1" number="1" />
+                <Card className="card-small card2" number="2" />
+            </div>
+            <Card className="card3" number="3" />
+            <Card className="card4" number="4" />
+        </div>
+        <Footer />
+    </div>
+);
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../style.css";
+
+
+const Header = () => (
+    <header style={{ backgroundColor: '#40d80d', padding: '2rem',
+        display: 'flex',
+        alignItems: 'center',
+        color: 'white'
+    }}>
+        <img src="https://porosfilkom.ub.ac.id/logo-poros.svg" alt="Poros Logo" style={{width: '100px'}}/>
+        <h1>Poros Incubation Week 2</h1>
+    </header>
+);
+
+export default Header;
+``
