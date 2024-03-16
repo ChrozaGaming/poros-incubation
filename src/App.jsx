8c7084c330adb7f15609748dbeebe53a61ab0723
ChrozaGaming <hilmyraihankindy@gmail.com>
2024-03-16 09:26:50 +0700

    fix mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Header from './components/Header';
+import Header from './components/Navbar.jsx';
 import Card from './components/Card';
 import Footer from './components/Footer';
 import './style.css';
